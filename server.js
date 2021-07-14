041d4588179e2abb2e1ff5a5be53c63f7d1c0e79
ErnestAr <arutiunian.ernest@gmail.com>
2021-07-14 17:28:27 -0400

    api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const workoutDB = require('./models/Workout');
 const app = express();
 const PORT =  3000;
 
@@ -17,6 +18,54 @@ app.get("/", (req, res) => {
 app.get("/exercise", (req, res) => res.sendFile(`${__dirname}/public/exercise.html`));
 
 app.get("/stats", (req, res) => res.sendFile(`${__dirname}/public/stats.html`));
+
+// Get (get last workout)
+app.get("/api/workouts", (req, res) => {
+  workoutDB.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+// Put (add exercise)
+app.put("/api/workouts/:id", ({body}, res) => {
+  workoutDB.create(body)
+    .then(({_id}) => workoutDB.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// Post (create workout)
+app.post("/api/workouts", ({body}, res) => {
+  workoutDB.Book.create(body)
+    .then(({_id}) => workoutDB.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// Get (get workout in range)
+app.get("/api/workouts/range", (req, res) => {
+  workoutDB.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
